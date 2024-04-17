257d0963e7086279842dd61c7b23042c1c5046ea
2001mapa <2001mapa@gmail.com>
2024-04-17 08:30:14 -0500

    nt9 Se crea proyecto ambiental con los diseños de VITE

---
+++ verdeoAPP/src/Home/Home.jsx
@@ -0,0 +1,11 @@
+import "./Home.css"
+export function Home(){
+    return(
+        <>
+            <section className="banner text-white">
+                <h2>VERDEO APP</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus tempora suscipit eum, molestias iste accusantium aut adipisci iure optio blanditiis?</p>
+            </section>          
+        </>
+    )
+}
